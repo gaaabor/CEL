b987e2529d962f05fa25fbdcbb1c9fc0fbd9604c
Gabor Bencsik <gaabor.bencsik@gmail.com>
2018-10-03 18:46:56 +0200

    updates

--- src/components/Teachers.js
+++ src/components/Teachers.js
@@ -2,12 +2,10 @@ import React from 'react'
 import Img from 'gatsby-image'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Teacher from './Teacher'
-
 const Teachers = () => (
   <StaticQuery
     query={graphql`
-      query teacherQuery {
+      query {
         magraoImage: file(relativePath: { eq: "magrao.jpg" }) {
           childImageSharp {
             fluid(maxWidth: 640, quality: 60) {
@@ -33,45 +31,62 @@ const Teachers = () => (
     `}
     render={data => (
       <section className="teacher">
-        <Teacher
-          item="magrao"
-          image={
+        <div className="teacher__item">
+          <div className="teacher__pic-box">
             <Img
               fluid={data.magraoImage.childImageSharp.fluid}
               className="teacher__pic"
             />
-          }
-          apelido="Instrutor Magrao"
-          name="Balogh Tamás"
-          text="Aliquam porta rizzle tellizzle. Mammasay mammasa mamma oo sa viverra, sapien fo dang hendrerizzle, libero urna hendrerit
-            leo, nizzle condimentum get down get down sapien at nunc. Donec eu fizzle. Vestibulum quis away."
-        />
-        <Teacher
-          item="prof"
-          image={
+          </div>
+          <div className="teacher__text-box">
+            <h1 className="heading-1 heading-1--green">Instrutor Magrao</h1>
+            <h2 className="heading-2">Balogh Tamás</h2>
+            <p className="teacher__para">
+              Aliquam porta rizzle tellizzle. Mammasay mammasa mamma oo sa
+              viverra, sapien fo dang hendrerizzle, libero urna hendrerit leo,
+              nizzle condimentum get down get down sapien at nunc. Donec eu
+              fizzle. Vestibulum quis away.
+            </p>
+          </div>
+        </div>
+
+        <div className="teacher__item">
+          <div className="teacher__pic-box">
             <Img
               fluid={data.profImage.childImageSharp.fluid}
               className="teacher__pic"
             />
-          }
-          apelido="Professor Cabeludo"
-          name="Oláh Krisztián"
-          text="Aliquam porta rizzle tellizzle. Mammasay mammasa mamma oo sa viverra, sapien fo dang hendrerizzle, libero urna hendrerit
-            leo, nizzle condimentum get down get down sapien at nunc. Donec eu fizzle. Vestibulum quis away."
-        />
-        <Teacher
-          item="sapi"
-          image={
+          </div>
+          <div className="teacher__text-box">
+            <h1 className="heading-1 heading-1--green">Professor Cabeludo</h1>
+            <h2 className="heading-2">Oláh Krisztián</h2>
+            <p className="teacher__para">
+              Aliquam porta rizzle tellizzle. Mammasay mammasa mamma oo sa
+              viverra, sapien fo dang hendrerizzle, libero urna hendrerit leo,
+              nizzle condimentum get down get down sapien at nunc. Donec eu
+              fizzle. Vestibulum quis away.
+            </p>
+          </div>
+        </div>
+
+        <div className="teacher__item">
+          <div className="teacher__pic-box">
             <Img
               fluid={data.sapiImage.childImageSharp.fluid}
               className="teacher__pic"
             />
-          }
-          apelido="Instrutor Sapinho"
-          name="Soós Levente"
-          text="Aliquam porta rizzle tellizzle. Mammasay mammasa mamma oo sa viverra, sapien fo dang hendrerizzle, libero urna hendrerit
-            leo, nizzle condimentum get down get down sapien at nunc. Donec eu fizzle. Vestibulum quis away."
-        />
+          </div>
+          <div className="teacher__text-box">
+            <h1 className="heading-1 heading-1--green">Instrutor Sapinho</h1>
+            <h2 className="heading-2">Soós Levente</h2>
+            <p className="teacher__para">
+              Aliquam porta rizzle tellizzle. Mammasay mammasa mamma oo sa
+              viverra, sapien fo dang hendrerizzle, libero urna hendrerit leo,
+              nizzle condimentum get down get down sapien at nunc. Donec eu
+              fizzle. Vestibulum quis away.
+            </p>
+          </div>
+        </div>
       </section>
     )}
   />
