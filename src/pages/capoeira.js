a78bdb1683fbef325badc50362b384880e9f9232
Gabor Bencsik <gaabor.bencsik@gmail.com>
2018-10-08 15:54:32 +0200

    updates

--- src/pages/capoeira.js
+++ src/pages/capoeira.js
@@ -1,35 +1,12 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Img from 'gatsby-image'
 import { StaticQuery, graphql } from 'gatsby'
+import NonPageComponent from '../components/nonPageComponent'
 
 const capoeiraPage = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        capoeiraImage: file(relativePath: { eq: "capoeira_square.jpg" }) {
-          childImageSharp {
-            fluid(maxWidth: 1920, quality: 70) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
-    render={data => {
-      console.log(data.capoeiraImage.childImageSharp.fluid)
-      return (
-        <div>
-          <h1 className="heading-1 heading-1--green">CAPOEIRA PAGE</h1>
-          <Img
-            fluid={data.capoeiraImage.childImageSharp.fluid}
-            style={{ height: '90vh' }}
-          />
-        </div>
-      )
-    }}
-  />
+  <Fragment>
+    <NonPageComponent />
+  </Fragment>
 )
 
 export default capoeiraPage
-
-// Building static HTML for pages{ capoeiraImage: { childImageSharp: { fluid: [Object] } } }
